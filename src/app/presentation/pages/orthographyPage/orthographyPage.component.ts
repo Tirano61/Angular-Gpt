f7ef331d609cd2fe45d6b21a9d95af91dbf2012b
Dario Ramirez <dariorama60@gmail.com>
2025-07-16 13:30:38 -0300

    Componentes de mensajes en ortography

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChatMessageComponent } from '@components/chats-bubbles/chatMessage/chatMessage.component';
+import { MyMessageComponent } from '@components/chats-bubbles/myMessage/myMessage.component';
+import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
+
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
   imports: [
     CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
