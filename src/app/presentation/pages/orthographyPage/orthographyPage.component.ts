30d03668bbc28c75bdc7f49f8d79b140e3e4f469
Dario Ramirez <dariorama60@gmail.com>
2025-07-17 20:09:57 -0300

    Terminando el selected para crear el template

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ChatMessageComponent } from '@components/chats-bubbles/chatMessage/chatMessage.component';
 import { MyMessageComponent } from '@components/chats-bubbles/myMessage/myMessage.component';
 import { TextMessageBoxComponent } from '@components/text-boxes/textMessageBox/textMessageBox.component';
 import { TextMessageBoxFileComponent, TextMessageEvent } from '@components/text-boxes/textMessageBoxFile/textMessageBoxFile.component';
+import { TextMessageBoxSelectEvent, TextMessageBoxSlectComponent } from '@components/text-boxes/textMessageBoxSlect/textMessageBoxSlect.component';
 import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
+import { MessageInterface } from '@interfaces/message.interface';
+
 
 
 @Component({
@@ -17,16 +20,23 @@ import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.com
     TypingLoaderComponent,
     TextMessageBoxComponent,
     TextMessageBoxFileComponent,
+    TextMessageBoxSlectComponent,
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrthographyPageComponent {
+  public messages = signal<MessageInterface[]>([{text: 'Hello, how can I help you?', isGpt: true}]);
+  public isLoading = signal(false);
+
   handleMessage(prompt: string) {
     console.log('Received message:', prompt);
   }
   handleMessageWithFile({ prompt, file }: TextMessageEvent) {
     console.log('Received message:', prompt);
     console.log('Received file:', file);
+  }
+  handleMessageWithSelect( event: TextMessageBoxSelectEvent ) {
+    console.log('Received message with select', event);
   }
 }
