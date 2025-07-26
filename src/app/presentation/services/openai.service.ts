0b4ac9d8340c3caf5169b56e70516d2990c9852d
Dario Ramirez <dariorama60@gmail.com>
2025-07-26 17:00:37 -0300

    Peticion y vista de audio en el chat

--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,6 +1,7 @@
 
 
 import { Injectable } from '@angular/core';
+import { textToAudioUseCase } from '@use-cases/audios/text-to-audio.use-case';
 import { orthographyUseCase } from '@use-cases/orthography/orthography.use-case';
 import { prosConsStreamUseCase } from '@use-cases/pros-cons/pros-cons-stream.use-case';
 import { prosConsUseCase } from '@use-cases/pros-cons/pros-cons.use-case';
@@ -24,6 +25,10 @@ export class OpenAiService {
 
     checkTranslate( prompt: string, lang: string){
         return  from ( translateUseCase( prompt, lang ));
+    }
+
+    textToAudio( prompt: string, voice: string ){
+        return from ( textToAudioUseCase( prompt, voice ));
     }
     
 }
