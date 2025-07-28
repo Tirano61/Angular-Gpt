8486af6671459f7d77711f4d4d3bf4216e93183b
Dario Ramirez <dariorama60@gmail.com>
2025-07-28 20:35:44 -0300

    Listo para recibir la transcripcion en el componente

--- src/app/presentation/pages/audioToTextPage/audioToTextPage.component.ts
+++ src/app/presentation/pages/audioToTextPage/audioToTextPage.component.ts
@@ -1,13 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent } from '@components/chats-bubbles/chatMessage/chatMessage.component';
+import { MyMessageComponent } from '@components/chats-bubbles/myMessage/myMessage.component';
+import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
+import { MessageInterface } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
+import { TextMessageBoxFileComponent, TextMessageEvent } from "@components/text-boxes/textMessageBoxFile/textMessageBoxFile.component";
 
 @Component({
   selector: 'app-audio-to-text-page',
   standalone: true,
   imports: [
     CommonModule,
-  ],
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxFileComponent
+],
   templateUrl: './audioToTextPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AudioToTextPageComponent { }
+export default class AudioToTextPageComponent { 
+  public messages = signal<MessageInterface[]>([]);
+      public isLoading = signal(false);
+      public openAiService = inject( OpenAiService );
+    
+      handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+        console.log('Received message:', prompt);
+        console.log('Received file:', file);
+      }
+      
+      
+}
