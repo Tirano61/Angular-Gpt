983959ee0ae86f7ae68106c8404471d244a3776f
Dario Ramirez <dariorama60@gmail.com>
2025-09-06 17:16:39 -0300

    Proncipio de image variation y image generatios

--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -3,6 +3,8 @@
 import { Injectable } from '@angular/core';
 import { audioToTextUseCase } from '@use-cases/audios/audio-to-text.use-case';
 import { textToAudioUseCase } from '@use-cases/audios/text-to-audio.use-case';
+import { imageGenerationUseCase } from '@use-cases/image-generation/image-generation.use-case';
+import { imageVariationUseCase } from '@use-cases/image-generation/image-variation.use-case';
 import { orthographyUseCase } from '@use-cases/orthography/orthography.use-case';
 import { prosConsStreamUseCase } from '@use-cases/pros-cons/pros-cons-stream.use-case';
 import { prosConsUseCase } from '@use-cases/pros-cons/pros-cons.use-case';
@@ -34,6 +36,14 @@ export class OpenAiService {
 
     audioToText(  audioFile: File, prompt?: string ){
         return from ( audioToTextUseCase( audioFile, prompt ));
+    }
+
+    imageGeneration( prompt: string, originalImage?: string, maskImage?: string ){
+        return from ( imageGenerationUseCase( prompt, originalImage, maskImage ));
+    }
+
+    imageVariation( originalImage: string ){
+        return from ( imageVariationUseCase( originalImage ));
     }
     
 }
